79e63cdeed84f939cb40043dc8dba70edce7f2d3
Gavin M. Roy <gavinmroy@gmail.com>
2015-07-29 17:29:51 -0400

    Fix the backup s3 copy

--- backup/backup.sh
+++ backup/backup.sh
@@ -2,4 +2,4 @@
 DATE=`date +%Y-%m-%d`
 MONTH=`date +%Y-%m`
 tar cvfj /tmp/backup-${DATE}.tar.bz2 /backup
-aws cp /tmp/backup-${DATE}.tar.bz2 s3://${BUCKET}/${MONTH}/backup-${DATE}.tar.bz2
+aws s3 cp /tmp/backup-${DATE}.tar.bz2 s3://${BUCKET}/${MONTH}/backup-${DATE}.tar.bz2
